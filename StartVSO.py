46ff11fe3fac2aafb81706b08fab2b82e9ce21d7
Ryozo <ryozokitada@gmail.com>
2020-03-25 07:27:39 +0000

    ADD_VSOの始め方

---
+++ StartVSO.py
@@ -0,0 +1,26 @@
+"""
+Visual Studio Onlineの始め方
+    1.Visual Studio Onlineって？
+        クラウド版IDEで、ネット環境さえあれば使えるVisual Studio Codeみたいなもの。
+        
+    2.始め方
+        公式サイトに飛ぶ
+        https://visualstudio.microsoft.com/ja/services/visual-studio-online/
+        [開始する] => [Create environment]
+        環境名、連携するGitのURLを入力
+        マシンタイプを選択
+        Createするとビルドが開始されるから1分くらい待つ
+        
+        後は拡張機能入れたりする。
+        ※ファイル作ったりすると。足りない拡張機能に関するポップアップが右下に出るから迷うことはない       
+        
+        ここまで5分程度
+
+    3.料金体系
+        接続している間は課金される
+
+        https://azure.microsoft.com/ja-jp/pricing/details/visual-studio-online/
+"""
+
+
+print("Hello, Visual Studio Online!")
